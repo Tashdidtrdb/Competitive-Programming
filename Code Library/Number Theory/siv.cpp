7509e469b3f0e2e481fee04d65e29d743f3c032a
Tashdidtrdb <tridibmzr@gmail.com>
2021-01-06 23:06:34 +0600

    siv update

--- Code Library/Number Theory/siv.cpp
+++ Code Library/Number Theory/siv.cpp
@@ -15,6 +15,7 @@ using namespace std;
 #define write freopen("vorta.txt","w",stdout);
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL);
 #define PI 2*acos(0.0)
+#define eps 1e-11
 #define DEBUG(x) cerr << #x << " = " << x << endl
 //#pragma comment(linker, "/stack:200000000")
 //#pragma GCC target ("avx2")
@@ -23,27 +24,52 @@ using namespace std;
 //#pragma GCC optimize("Ofast")
 //#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 
-const int MAX = 1e5 + 5, MOD = 1e9 + 7  , MAXLG = log2(MAX)+1;
+const int MAX = 2e5 + 5, MOD = 1e9 + 7  , MAXLG = log2(MAX)+1;
 const ll inf = 1e18 + 5;
 
-vector<bool>mark(MAX,1);
-vector<int>prime;
-int siv(){
-    int total=0;
-    mark[1]=0;
+int N;
+vector<bool> mark(N, true);
+vector<int> prime;
+void siv(){
     prime.pb(2);
-    total++;
-    for(ll i=4; i<MAX; i+=2) mark[i]=0;
-    for(ll i=3; i<MAX; i+=2){
-        if(mark[i]){
-            prime.pb(i);
-            total++;
-            if(i*i < MAX){
-                for(ll j = i*i; j < MAX; j += 2*i){
-                    mark[j] = 0;
-                }
-            }
+    for(int i = 4; i < N; i += 2) mark[i] = false;
+    for(ll i = 3; i < N; i += 2){
+        if(!mark[i]) continue;
+        prime.pb(i);
+        for(ll j = i*i; j < N; j += 2*i) mark[j] = false;
+    }
+}
+
+// linear complexity
+const int N;
+int spf[N];
+vector<int> pr;
+
+void siv(){
+    for(int i = 2; i < N; i++){
+        if(spf[i] == 0) spf[i] = i, pr.pb(i);
+        for(int j = 0; j < pr.size() && pr[j] <= spf[i] && i * pr[j] < N; j++){
+            spf[i * pr[j]] = pr[j];
         }
     }
-    return total;
+}
+
+vector<int> factorize(int n){
+    vector<int> ret;
+    while(n > 1){
+        int div = spf[n], cnt = 0;
+        while( !(n % div) ){
+            cnt++;
+            n /= div;
+        }
+        ret.pb(cnt);
+    }
+    return ret;
+}
+
+int arr[MAX];
+int main(){
+
+    fastio;
+            
 }
