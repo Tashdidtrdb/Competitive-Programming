5879cb6ad87ac8e36d6089a3106a9586a0630040
Mirza Zamiur Rahman <tridibmzr@gmail.com>
2022-09-04 04:09:10 +0600

    prefix automaton

--- Code Library/String/kmp.cpp
+++ Code Library/String/kmp.cpp
@@ -23,7 +23,7 @@ const int MAX = 2e5 + 5, MOD = 1e9 + 7;
 const ll inf = 1e18 + 5;
 
 vector<int> prefix_function(string s) {
-  int n = (int)s.length();
+  int n = (int) s.size();
   vector<int> pi(n);
   for (int i = 1; i < n; i++) {
     int j = pi[i - 1];
@@ -32,6 +32,30 @@ vector<int> prefix_function(string s) {
     pi[i] = j;
   }
   return pi;
+}
+
+vector<vector<int>> prefix_automaton(string s) {
+  s += '#';
+  int n = s.size();
+  vector<int> pi = prefix_function(s);
+  vector<vector<int>> automaton(n, vector<int>(26));
+  for (int i = 0; i < n; i++) {
+    for (int c = 0; c < 26; c++) {
+      if (i > 0 && c + 'a' != s[i]) automaton[i][c] = automaton[pi[i-1]][c];
+      else automaton[i][c] = i + (c + 'a' == s[i]);
+    }
+  }
+  return automaton;
+}
+
+void add(vector<int> &pi, vector<vector<int>> &automaton, char ch) {
+  int n = pi.size();
+  automaton.pb(vector<int>(26));
+  for (int c = 0; c < 26; c++) {
+    if (c + 'a' != ch) automaton[n][c] = automaton[pi.back()][c];
+    else automaton[n][c] = n + (c + 'a' == ch);
+  }
+  pi.pb(automaton[pi.back()][ch - 'a']);
 }
 
 int arr[MAX];
